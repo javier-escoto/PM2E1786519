412fe91bd7ac1a6fb174c4ec356657c96a9ff948
javie <javie@DESKTOP-KAOB9VG>
2023-02-20 22:38:22 -0600

    Add project files.

---
+++ PM2E1786519/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using PM2E1786519.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM2E1786519
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new PageMapa();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PM2E1786519/View/PageMapa.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace PM2E1786519.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageMapa : ContentPage
+	{
+		public PageMapa ()
+		{
+			InitializeComponent ();
+		}
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            Pin pin = new Pin
+            {
+                Label = ubicacion.Text,
+                Address = ubicacion_corta.Text,
+                Type = PinType.Generic,
+                Position = new Position(Convert.ToDouble(latitud.Text), Convert.ToDouble(longitud.Text))
+            };
+
+            mapa.Pins.Add(pin);
+            mapa.MoveToRegion(mapSpan: MapSpan.FromCenterAndRadius(new Position(Convert.ToDouble(latitud.Text), Convert.ToDouble(longitud.Text)), Distance.FromKilometers(1)));
+
+        }
+        }
+    }
